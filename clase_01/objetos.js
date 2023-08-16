5d9e19f99e895f2bb1be3ad41f6b9c1f45b43097
matuteblack <112114748+matuteblack@users.noreply.github.com>
2023-08-15 22:43:44 -0300

    feat: add object with setter and getter

--- clase_01/objetos.js
+++ clase_01/objetos.js
@@ -1,1 +1,16 @@
-console.log('Hola mundo');
+const persona = {
+    firstName: 'Matías',
+    lastName: 'Quinteros',
+    age: 23,
+
+    getFirstName: function () {
+        return this.firstName;
+    },
+    setFirstName: function (newFirstName) {
+        this.firstName = newFirstName;
+    }
+}
+
+console.log(persona.getFirstName());
+persona.setFirstName('Matías Agustin')
+console.log(persona.getFirstName());
