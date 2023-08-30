a076e0b10886ac208de708158423382dc9310ebe
matuteblack <112114748+matuteblack@users.noreply.github.com>
2023-08-29 22:32:30 -0300

    add: class Serie

---
+++ clase_03/serie.js
@@ -0,0 +1,30 @@
+class Serie {
+    title  = '';
+    imgUrl = '';
+    year   = '';
+
+    constructor() {
+
+    }
+
+    setTitle(title) {
+        this.title = title;
+    }
+    getTitle() {
+        return this.title;
+    }
+
+    setImgUrl(imgUrl) {
+        this.imgUrl= imgUrl;
+    }
+    getImgUrl() {
+        return this.imgUrl;
+    }
+
+    setYear() {
+        this.year = year;
+    }
+    getYear() {
+        return this.year;
+    }
+}
