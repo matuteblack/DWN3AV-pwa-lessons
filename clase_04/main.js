7b7558f41be20037a46bc67ce143b373061bf5cb
matuteblack <112114748+matuteblack@users.noreply.github.com>
2023-09-30 21:38:09 -0300

    clase 03: nueva promesa

--- clase_04/main.js
+++ clase_04/main.js
@@ -1,12 +1,25 @@
 // timeout integrado
-setTimeout(() => {
-    console.log('...2 segundos después');
-}, 2000);
+// setTimeout(() => {
+//     console.log('...2 segundos después');
+// }, 2000);
 
 // timeout armado desde fuera
-const tiempo_retraso = 3000;
+const tiempo_retraso = 2000;
     // función callback
 const action = () => console.log('...3 segundos después');
 
-setTimeout(action, tiempo_retraso);
 
+// Promesas
+
+console.log('Comienzo de app...');
+
+const infoServer = new Promise((done) => {
+    setTimeout(() => {
+        done(12);
+    }, tiempo_retraso);
+});
+
+infoServer
+    .then(value => {
+        console.log(value);
+    })
