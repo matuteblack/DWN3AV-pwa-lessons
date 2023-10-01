1c8062cebddff9400a6f13f2c9b8b070bcaee474
matuteblack <112114748+matuteblack@users.noreply.github.com>
2023-10-01 18:44:36 -0300

    clase 05: nueva promesa

--- clase_05/main.js
+++ clase_05/main.js
@@ -1,3 +1,4 @@
+// Lista de personas
 const peopleList = [
     {
         firstName: 'Matías',
@@ -6,47 +7,119 @@ const peopleList = [
         age: 23
     },
     {
-        firstName: 'Fernando',
-        lastName: 'Aveledo',
-        title: 'Mr.',
-        age: 23
+        firstName: 'Ana',
+        lastName: 'González',
+        title: 'Ms.',
+        age: 30
     },
     {
-        firstName: 'Clara',
-        lastName: 'Jorge',
+        firstName: 'Carlos',
+        lastName: 'Pérez',
+        title: 'Mr.',
+        age: 45
+    },
+    {
+        firstName: 'Laura',
+        lastName: 'Martínez',
         title: 'Mrs.',
-        age: 24
+        age: 28
+    },
+    {
+        firstName: 'Juan',
+        lastName: 'López',
+        title: 'Mr.',
+        age: 35
+    },
+    {
+        firstName: 'Sofía',
+        lastName: 'Hernández',
+        title: 'Ms.',
+        age: 26
+    },
+    {
+        firstName: 'Gabriel',
+        lastName: 'Ramírez',
+        title: 'Mr.',
+        age: 40
+    },
+    {
+        firstName: 'María',
+        lastName: 'Gómez',
+        title: 'Mrs.',
+        age: 32
+    },
+    {
+        firstName: 'Alejandro',
+        lastName: 'Díaz',
+        title: 'Mr.',
+        age: 38
+    },
+    {
+        firstName: 'Isabel',
+        lastName: 'Torres',
+        title: 'Ms.',
+        age: 27
     }
 ];
 
-const valid = false;
-
-const generateName = (people) => {
-    return `${people.title} ${people.firstName} ${people.lastName}`;
-}
-const generatePerson = (person) => {
-    const {age, ...other} = person;
-    return {
-        age,
-        name: generateName(other)
-    }
-}
-
+const valid = true;
 
 const peoplePromise = new Promise((done, reject) => {
     setTimeout(() => {
-        if(valid) {
-            done(peopleList);
+        if (valid) {
+            done(peopleList)
         } else {
-            reject({message: 'invalid request'});
+            reject({messaje: 'invalid request'});
         }
-    }, 3000);
-});
+    }, 2000);
+})
 
-peoplePromise
-    .then((people) => people.map(generatePerson))
-    .catch((error) => {
-        console.error(error)
-        return []
-    })
-    .then((people) => console.log(people));
+peoplePromise.then((people => console.log(people)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const valid = false;
+
+// const generateName = (people) => {
+//     return `${people.title} ${people.firstName} ${people.lastName}`;
+// }
+// const generatePerson = (person) => {
+//     const {age, ...other} = person;
+//     return {
+//         age,
+//         name: generateName(other)
+//     }
+// }
+
+
+// const peoplePromise = new Promise((done, reject) => {
+//     setTimeout(() => {
+//         if(valid) {
+//             done(peopleList);
+//         } else {
+//             reject({message: 'invalid request'});
+//         }
+//     }, 3000);
+// });
+
+// peoplePromise
+//     .then((people) => people.map(generatePerson))
+//     .catch((error) => {
+//         console.error(error)
+//         return []
+//     })
+//     .then((people) => console.log(people));
