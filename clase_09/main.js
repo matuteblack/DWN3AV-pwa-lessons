3dcb7af724283c51aa03f683c6003a6e7a89808e
matuteblack <112114748+matuteblack@users.noreply.github.com>
2023-10-14 21:45:25 -0300

    clase 09: setup

--- clase_09/main.js
+++ clase_09/main.js
@@ -2,7 +2,22 @@ const app = new Vue({
     el: '#app',
     data: {
         titulo: 'Hola mundo',
-        
+        frutas: [
+            {
+                nombre: 'manzana',
+                cantidad: 2,
+            },
+            {
+                nombre: 'pera',
+                cantidad: 2,
+            },            {
+                nombre: 'banana',
+                cantidad: 2,
+            },            {
+                nombre: 'uva',
+                cantidad: 2,
+            },
+        ]
     }
 });
 
