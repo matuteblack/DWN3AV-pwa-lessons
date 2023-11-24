113aa69c2ec26803724365b12af3f91c19c4cfbe
matuteblack <112114748+matuteblack@users.noreply.github.com>
2023-11-24 04:58:05 -0300

    clase 10: guardar archivos en cache

--- clase_10/main.js
+++ clase_10/main.js
@@ -2,10 +2,10 @@ console.log('working');
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js')
     .then((registration) => {
-        console.log('service worker registrado');
+        console.log('service worker registrado -', registration);
     })
     .catch((error) => {
-        console.log('service worker no registrado');
+        console.log('service worker no registrado -', error);
     });
 }
 
--- clase_10/sw.js
+++ clase_10/sw.js
@@ -11,29 +11,45 @@ const animales = [
     'vaca'
 ];
 
-self.addEventListener('message', (e) => {
-    console.log('message recieved from main.js');
-    const isInAnimalList = animales.includes(e.data);
-    if(isInAnimalList) {
-        setTimeout(() => {
-            self.postMessage({
-                status: 'ok',
-                message: 'animal found'
-            })
-        }, 2000);
-    } else {
-        self.postMessage({
-            status: 'error',
-            message: 'animal not found'
-        })
-    }
-})
+// self.addEventListener('message', (e) => {
+//     console.log('message recieved from main.js');
+//     const isInAnimalList = animales.includes(e.data);
+//     if(isInAnimalList) {
+//         setTimeout(() => {
+//             self.postMessage({
+//                 status: 'ok',
+//                 message: 'animal found'
+//             });
+//         }, 2000);
+//     } else {
+//         self.postMessage({
+//             status: 'error',
+//             message: 'animal not found'
+//         });
+//     }
+// })
 
 
 self.addEventListener('install', (e) => {
     console.log('service worker instalado');
+    caches.open('mi-cache-1').then((cache) => {
+        cache.addAll([
+            'index.html',
+            'main.js'
+        ]);
+    });
 });
 
 self.addEventListener('fetch', (e) => {
     console.log('fetch trigger', e);
+    caches.has('mi-cache-1')
+    .then(respuesta => {
+        caches.open('mi-cache-1')
+        .then(cache => {
+            cache.match('main.js')
+            .then(respuesta => {
+                console.log('archivo cacheado', respuesta);
+            });
+        });
+    });
 });
